617237a4b643e7da0ed467e8d2e019415216d4f4
youssefoubens <oubenssaidyoussef@gmail.com>
2025-05-27 16:38:03 +0100

    create project

---
+++ src/main/java/com/zenika/vendingmachine/ZenikaApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.vendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenikaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zenika/vendingmachine/ZenikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
